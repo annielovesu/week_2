29edf5f67c763bf9b7435af14894ae59bdc9f9c2
annielovesu <annielovesu1@gmail.com>
2020-11-07 09:53:02 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+def print_h1(name):
+    print(f'What is your name? {name}')
+
+
+if __name__ == '__main__':
+    print_h1('Amanda')
+
+
+def print_h2(age):
+    print(f'How old are you? {age}')
+
+
+if __name__ == '__main__':
+    print_h2('15')
+
+
+def print_h3(name):
+    print(f'Hello Amanda! You were born in {name}')
+
+if __name__ == '__main__':
+    print_h3('2005')
